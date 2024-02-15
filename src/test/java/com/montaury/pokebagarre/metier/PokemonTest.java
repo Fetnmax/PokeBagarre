ac0175691fa1aeb8b3950ffdf4d2ddd4f9d7724a
Maxime Jacob-Saussereau <maximejacob4000@gmail.com>
2024-02-15 12:01:53 +0100

    test unitaire

---
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -0,0 +1,72 @@
+package com.montaury.pokebagarre.metier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+    @Test
+    void le_premier_pokemon_devrait_gagner_le_deuxieme_grace_a_l_attaque() {
+        // Given
+        Pokemon premierPokemon = new Pokemon("Samuel", "samuel.png", new Stats(10, 5));
+        Pokemon deuxiemePokemon = new Pokemon("Mewtwo", "mewtwo.png", new Stats(5, 5));
+
+        // When
+        boolean resultat = premierPokemon.estVainqueurContre(deuxiemePokemon);
+
+        // Then
+        assertTrue(resultat);
+    }
+
+    @Test
+    void le_deuxieme_pokemon_devrait_gagner_le_premier_grace_a_l_attaque() {
+        // Given
+        Pokemon premierPokemon = new Pokemon("Samuel", "samuel.png", new Stats(5, 5));
+        Pokemon deuxiemePokemon = new Pokemon("Mewtwo", "mewtwo.png", new Stats(10, 5));
+
+        // When
+        boolean resultat = premierPokemon.estVainqueurContre(deuxiemePokemon);
+
+        // Then
+        assertFalse(resultat);
+    }
+
+    @Test
+    void le_premier_pokemon_devrait_gagner_le_deuxieme_grace_a_la_defense() {
+        // Given
+        Pokemon premierPokemon = new Pokemon("Samuel", "samuel.png", new Stats(10, 5));
+        Pokemon deuxiemePokemon = new Pokemon("Mewtwo", "mewtwo.png", new Stats(10, 4));
+
+        // When
+        boolean resultat = premierPokemon.estVainqueurContre(deuxiemePokemon);
+
+        // Then
+        assertTrue(resultat);
+    }
+
+    @Test
+    void le_deuxieme_pokemon_devrait_gagner_le_premier_grace_a_la_defense() {
+        // Given
+        Pokemon premierPokemon = new Pokemon("Samuel", "samuel.png", new Stats(10, 4));
+        Pokemon deuxiemePokemon = new Pokemon("Mewtwo", "mewtwo.png", new Stats(10, 5));
+
+        // When
+        boolean resultat = premierPokemon.estVainqueurContre(deuxiemePokemon);
+
+        // Then
+        assertFalse(resultat);
+    }
+
+    @Test
+    void le_premier_pokemon_devrait_gagner_le_deuxieme_grace_a_sa_position() {
+        // Given
+        Pokemon premierPokemon = new Pokemon("Samuel", "samuel.png", new Stats(10, 10));
+        Pokemon deuxiemePokemon = new Pokemon("Mewtwo", "mewtwo.png", new Stats(10, 10));
+
+        // When
+        boolean resultat = premierPokemon.estVainqueurContre(deuxiemePokemon);
+
+        // Then
+        assertTrue(resultat);
+    }
+}
